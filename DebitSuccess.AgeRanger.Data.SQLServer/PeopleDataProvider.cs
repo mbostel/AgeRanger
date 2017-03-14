d3464c3c624f0677f998259be7accb5e2f6db1c4
mbostel <mark.bostel.nz@gmail.com>
2017-03-14 14:16:37 +1300

    Initial commit

---
+++ DebitSuccess.AgeRanger.Data.SQLServer/PeopleDataProvider.cs
@@ -0,0 +1,42 @@
+﻿using DebitSuccess.AgeRanger.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DebitSuccess.AgeRanger.Data.SQLServer {
+    public class PeopleDataProvider : IPeopleDataProvider {
+
+        public int CreatePerson(IPersonEntity person) {
+            throw new NotImplementedException();
+        }
+
+        public int DeletePerson(int ID) {
+            throw new NotImplementedException();
+        }
+
+        public string GetAgeGroup(int age) {
+            throw new NotImplementedException();
+        }
+
+        public IPersonEntity GetPerson(int ID) {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IPersonEntity> ListPeople() {
+            return ListPeople(null);
+        }
+
+        public IEnumerable<IPersonEntity> ListPeople(IPeopleFilter filter) {
+            throw new NotImplementedException();
+        }
+
+        public int UpdatePeople(IEnumerable<IPersonEntity> people) {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
